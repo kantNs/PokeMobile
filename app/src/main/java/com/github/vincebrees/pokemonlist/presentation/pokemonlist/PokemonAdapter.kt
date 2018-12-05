301105f4f3ff4c9418790cd9ca21959d4b432ab5
kantNS <thebosssdu69@gmail.com>
2018-12-05 22:37:17 +0100

    Add on click with a toast to show the selected pokemon
    Signed-off-by: kantNS <thebosssdu69@gmail.com>

--- app/src/main/java/com/github/vincebrees/pokemonlist/presentation/pokemonlist/PokemonAdapter.kt
+++ app/src/main/java/com/github/vincebrees/pokemonlist/presentation/pokemonlist/PokemonAdapter.kt
@@ -1,10 +1,14 @@
 package com.github.vincebrees.pokemonlist.presentation.pokemonlist
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import com.bumptech.glide.Glide
 import com.github.vincebrees.pokemonlist.R
 import com.github.vincebrees.pokemonlist.domain.Pokemon
@@ -27,12 +31,20 @@ class PokemonAdapter(val context: Context, var listModel: List<Pokemon>) : Recyc
 
 
     class ViewHolder(val context: Context, itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+
         fun bindItem(model: Pokemon) {
             with(itemView) {
                 item_pokemon_name.text = model.name
                 Glide.with(context)
                     .load(model.img)
                     .into(item_pokemon_img);
+                itemView.setOnClickListener {
+                    Toast.makeText(context, model.name,LENGTH_SHORT).show();
+                   // LocalBroadcastManager.getInstance(context)
+                    //    .sendBroadcast(Intent(Common.KEY_ENABLE_HOME).putExtra("name",model.name))
+                    }
             }
         }
     }
